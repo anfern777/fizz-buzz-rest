a4aa9082833dadfadb73e388696d91a12aa5dd29
André Fernandes <anfern.pro@gmail.com>
2025-07-23 12:30:35 +0200

    feat: healthcheck handler logic

--- healthcheck.go
+++ healthcheck.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "healthcheck endpoint")
+	type healthcheck struct {
+		Environment string `json:"env"`
+		Status      string `json:"status"`
+	}
+
+	health := &healthcheck{
+		Environment: app.config.env,
+		Status:      "available",
+	}
+
+	if err := app.toJsonResponse(w, http.StatusOK, health, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
