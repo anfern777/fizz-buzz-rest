ee4708eaffecbf2fba38d8c67305fc9f29bee428
André Fernandes <anfern.pro@gmail.com>
2025-07-26 17:05:58 +0200

    feat: add version to healthcheck endpoint

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -6,13 +6,15 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	type healthcheck struct {
-		Environment string `json:"env"`
 		Status      string `json:"status"`
+		Environment string `json:"env"`
+		Version     string `json:"version"`
 	}
 
 	health := &healthcheck{
-		Environment: app.config.env,
 		Status:      "available",
+		Environment: app.config.env,
+		Version:     version,
 	}
 
 	err := app.toJSONResponse(w, http.StatusOK, &wrapper{"health": health}, nil)
